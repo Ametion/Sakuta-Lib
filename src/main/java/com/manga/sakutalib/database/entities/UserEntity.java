97160e4a75aa952b6edc2e71f5a456742b0546fd
yehor kochetov <yehor.kochetov@gmail.com>
2023-01-26 20:03:47 +0100

    add user entity adn repository to it

---
+++ src/main/java/com/manga/sakutalib/database/entities/UserEntity.java
@@ -0,0 +1,73 @@
+package com.manga.sakutalib.database.entities;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "users")
+public class UserEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String firstName;
+
+    @Column(nullable = false)
+    private String secondName;
+
+    @Column(nullable = false)
+    private String nickName;
+
+    @Column(nullable = false, unique = true)
+    private String login;
+
+    @Column(nullable = false)
+    private String password;
+
+    public UserEntity() { }
+
+    public UserEntity(String firstName, String secondName, String nickName, String login, String password) {
+        this.firstName = firstName;
+        this.secondName = secondName;
+        this.nickName = nickName;
+        this.login = login;
+        this.password = password;
+    }
+
+    public UserEntity(Long id, String firstName, String secondName, String nickName, String login, String password) {
+        this.id = id;
+        this.firstName = firstName;
+        this.secondName = secondName;
+        this.nickName = nickName;
+        this.login = login;
+        this.password = password;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getSecondName() {
+        return secondName;
+    }
+
+    public String getNickName() {
+        return nickName;
+    }
+
+    public void setNickName(String nickName) {
+        this.nickName = nickName;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+}
---
+++ src/main/java/com/manga/sakutalib/database/repositories/UserRepository.java
@@ -0,0 +1,6 @@
+package com.manga.sakutalib.database.repositories;
+
+import com.manga.sakutalib.database.entities.UserEntity;
+import org.springframework.data.repository.CrudRepository;
+
+public interface UserRepository extends CrudRepository<UserEntity, Long> { }
